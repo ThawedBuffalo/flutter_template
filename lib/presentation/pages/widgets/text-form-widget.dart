bc22bf8bb750c7568a4168a0594c14c84456aa5f
JC <thawedbuffalo@gmail.com>
2023-08-30 07:28:04 -0400

    adding custom widget

---
+++ lib/presentation/pages/widgets/text-form-widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class TextFormWidget extends StatelessWidget {
+
+  String? title;
+  IconData? leadingIcon, trailingIcon;
+  Color? inputColor, iconColor;
+
+  TextFormWidget({
+    required this.title,
+    this.leadingIcon,
+    this.trailingIcon,
+    this.inputColor,
+    this.iconColor
+});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ,
+    );
+  }
+}
