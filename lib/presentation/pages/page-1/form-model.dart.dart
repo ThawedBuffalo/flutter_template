2627e5ee8e7d92c6ee3304ce422eef186aeb50b5
JC <thawedbuffalo@gmail.com>
2023-09-04 07:41:49 -0400

    completed form, adding model

--- lib/presentation/pages/page-1/form-model.dart.dart
+++ lib/presentation/pages/page-1/form-model.dart.dart
@@ -1,35 +1,38 @@
 import 'package:equatable/equatable.dart';
 
+// NOTE: this file would normally live in the clean arch model layer, here just for demo
+
 // models are the direct mapping from repository/apis
 // entities are the logical models
 
 class FormModel extends Equatable {
   final String productName;
   final String productDescription;
+  final String? optionalPlaceholder;
 
   const FormModel(
-      {required this.placeholderId,
-      required this.placeholderName,
+      {required this.productName,
+      required this.productDescription,
       required this.optionalPlaceholder});
 
   factory FormModel.fromJson(Map<String, dynamic> jsonData) {
     Map<String, dynamic> jsonUserData = jsonData['placeholder_jsontitle'];
 
     return FormModel(
-        placeholderId: jsonUserData['placeholderId'],
-        placeholderName: jsonUserData['placeholderName'],
+        productName: jsonUserData['placeholderId'],
+        productDescription: jsonUserData['placeholderName'],
         optionalPlaceholder: jsonUserData['optionalPlaceholder']);
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'placeholderId': placeholderId,
-      'placeholderName': placeholderName,
+      'placeholderId': productName,
+      'placeholderName': productDescription,
       'optionalPlaceholder': optionalPlaceholder
     };
   }
 
   @override
   List<Object?> get props =>
-      [placeholderId, placeholderName, optionalPlaceholder];
+      [productName, productDescription, optionalPlaceholder];
 }
